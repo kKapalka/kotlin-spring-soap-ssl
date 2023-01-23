a90ddcbb9b5cc4f69c392cb8016ce27dc5070678
kKapalka <k.kapalka@hotmail.com>
2023-01-23 15:29:15 +0100

    initialize

---
+++ src/main/kotlin/com/kkapalka/soapserver/SoapServerApplication.kt
@@ -0,0 +1,11 @@
+package com.kkapalka.soapserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoapServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SoapServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/kkapalka/soapserver/SoapServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kkapalka.soapserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoapServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
